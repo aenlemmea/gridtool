040dfb86aef1d5b3e8f39bf4e3627c10d542e8ac
aenlemmea <118059484+aenlemmea@users.noreply.github.com>
2023-10-17 17:47:23 +0530

    Added the editable grid concept

--- src/main.py
+++ src/main.py
@@ -1,21 +1,39 @@
 import sys
 
-from PyQt6.QtWidgets import QApplication, QMainWindow
+from PyQt6.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QPushButton, QWidget, QHBoxLayout, QLineEdit, \
+    QGridLayout
 
-from src.draw_grid import DrawGrid
+WIDTH = 800
+HEIGHT = 600
 
 
 class MainWindow(QMainWindow):
 
     def __init__(self):
         super().__init__()
-        self.gridframe = DrawGrid()
+
+        layout = QGridLayout()
+
+        toolbar = QHBoxLayout()
+
+        toolbar.addWidget(QPushButton('Infinite Grid'))
+        toolbar.addWidget(QPushButton('Give Dimensions'))
+
+        self.user_text_box = [[] for _ in range(5)]
+        positions = [(i + 1, j + 1) for i in range(5) for j in range(5)]
+
+        for i, position in enumerate(positions):
+            self.user_text_box[position[0] - 1].append(QLineEdit())
+            layout.addWidget(self.user_text_box[position[0] - 1][position[1] - 1], *position)
+
+        dummy_widget = QWidget()
+        dummy_widget.setLayout(layout)
+        self.setCentralWidget(dummy_widget)
         self.initialize()
 
     def initialize(self):
-        self.setGeometry(200, 800, 700, 500)
+        self.setGeometry(0, 0, WIDTH, HEIGHT)
         self.setWindowTitle("Grid Tool")
-        self.setCentralWidget(self.gridframe)
         self.show()
 
 
